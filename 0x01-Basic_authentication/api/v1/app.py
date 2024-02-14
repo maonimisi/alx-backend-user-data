1e70d61ce93e1964192917847615040569cc6ae8
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-02-14 22:48:18 +0100

    Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,9 +16,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 auth = None
-if genenv("AUTH_TYPE", None) == "auth":
+if getenv("AUTH_TYPE", None) == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+
 
 if getenv("AUTH_TYPE", None) == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
