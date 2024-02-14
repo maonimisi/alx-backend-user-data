6e17cda01c4360dea93d87eb6bc59a724226144a
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-02-14 21:09:57 +0100

    Error Handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """ abort with 401 error"""
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ abort with 403 error"""
+    abort(403)
