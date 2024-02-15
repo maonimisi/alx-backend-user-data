9fa73b0b0ef4771b645255cbe5d47fff2875e0bc
Muhammed Abdulrasheed Onimisi <maonimisi>
2024-02-15 07:52:37 +0100

    Basic Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,12 +13,26 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
-        """
-        Performs base64 encoding on the authorization_header
-        extract base64 of authorization header after "Basic "
+        """Returns the Base64 part of the Authorization header for a
+           Basic Authentication
         """
         if authorization_header is None or type(authorization_header) != str:
             return None
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split("Basic ")[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns the decoded value of a Base64 string
+           base64_authorization_header
+        """
+        if base64_authorization_header is None or \
+           type(base64_authorization_header) != str:
+            return None
+
+        try:
+            return base64.b64decode(base64_authorization_header).\
+                decode('utf-8')
+        except Exception:
+            return None
